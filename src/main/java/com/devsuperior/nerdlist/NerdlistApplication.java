70f062dc28f04fc39e3e3096cd010ef4271b19fb
Alexandre Direito <xandedsj@gmail.com>
2025-05-19 20:32:39 -0300

    Projeto Criado

---
+++ src/main/java/com/devsuperior/nerdlist/NerdlistApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.nerdlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NerdlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NerdlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/nerdlist/NerdlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.nerdlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NerdlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
