979dd8f95f37d891e40f427eac5a427d2939977c
Alexandre Direito <xandedsj@gmail.com>
2025-05-20 21:16:58 -0300

    GET games by lists

---
+++ src/main/java/com/devsuperior/nerdlist/projections/GameMinProjection.java
@@ -0,0 +1,11 @@
+package com.devsuperior.nerdlist.projections;
+
+public interface GameMinProjection {
+
+    Long getId();
+    String getTitle();
+    Integer getYear();
+    String getImgUrl();
+    String getShortDescription();
+    Integer getPosition();
+}
--- src/main/java/com/devsuperior/nerdlist/services/GameService.java
+++ src/main/java/com/devsuperior/nerdlist/services/GameService.java
@@ -3,6 +3,7 @@ package com.devsuperior.nerdlist.services;
 import com.devsuperior.nerdlist.dto.GameDTO;
 import com.devsuperior.nerdlist.dto.GameMinDTO;
 import com.devsuperior.nerdlist.entities.Game;
+import com.devsuperior.nerdlist.projections.GameMinProjection;
 import com.devsuperior.nerdlist.repositories.GameRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +27,13 @@ public class GameService {
     @Transactional()
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
+        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+        return dto;
+    }
+
+    @Transactional()
+    public List<GameMinDTO> findByList(Long listId) {
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
         List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
         return dto;
     }
